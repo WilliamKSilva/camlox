497ca03cf8aeb93cabc7201f93b6be20a6dcaecb
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-14 15:52:03 -0300

    feat: add the definition of a Token

---
+++ bin/token.ml
@@ -0,0 +1,55 @@
+type token_type =
+  (* Single-character tokens *)
+  | LEFT_PAREN
+  | RIGHT_PAREN
+  | LEFT_BRACE
+  | RIGHT_BRACE
+  | COMMA
+  | DOT
+  | MINUS
+  | PLUS
+  | SEMICOLON
+  | SLASH
+  | STAR
+  (* One or two characters tokens *)
+  | BANG
+  | BANG_EQUAL
+  | EQUAL
+  | EQUAL_EQUAL
+  | GREATER
+  | GREATER_EQUAL
+  | LESS
+  | LESS_EQUAL
+  (* Literals *)
+  | IDENTIFIER
+  | STRING
+  | NUMBER
+  (* Keywords *)
+  | AND
+  | CLASS
+  | ELSE
+  | FALSE
+  | FUN
+  | FOR
+  | IF
+  | OR
+  | PRINT
+  | RETURN
+  | SUPER
+  | THIS
+  | TRUE
+  | VAR
+  | WHILE
+  | EOF
+
+type literal =
+  | IntLiteral of int
+  | StrLiteral of string
+  | FloatLiteral of float
+
+type token = {
+  token_type : token_type;
+  lexeme : string;
+  literal : literal;
+  line : int;
+}
