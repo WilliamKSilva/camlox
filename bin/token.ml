3a5b42cfab3e842f0d9f140e8e6439c0c62e1989
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-16 14:37:45 -0300

    feat: add a function to retrieve a Hashtbl of token_types that are queried by the reserved word string

--- bin/token.ml
+++ bin/token.ml
@@ -40,6 +40,7 @@ type token_type =
   | TRUE
   | VAR
   | WHILE
+  | NIL
   | EOF
 
 type literal = StrLiteral of string | NumLiteral of float | None
@@ -52,3 +53,24 @@ type token = {
 }
 
 type identifiers = (string, token_type) Hashtbl.t
+
+(* TODO: This probably can be cleaner, maybe map token_type some way? *)
+let get_identifiers : identifiers =
+  let hashtbl = Hashtbl.create 38 in
+  Hashtbl.add hashtbl "and" AND;
+  Hashtbl.add hashtbl "class" CLASS;
+  Hashtbl.add hashtbl "else" ELSE;
+  Hashtbl.add hashtbl "false" FALSE;
+  Hashtbl.add hashtbl "for" FOR;
+  Hashtbl.add hashtbl "fun" FUN;
+  Hashtbl.add hashtbl "if" IF;
+  Hashtbl.add hashtbl "nil" NIL;
+  Hashtbl.add hashtbl "or" OR;
+  Hashtbl.add hashtbl "print" PRINT;
+  Hashtbl.add hashtbl "return" RETURN;
+  Hashtbl.add hashtbl "super" SUPER;
+  Hashtbl.add hashtbl "this" THIS;
+  Hashtbl.add hashtbl "true" TRUE;
+  Hashtbl.add hashtbl "var" VAR;
+  Hashtbl.add hashtbl "while" WHILE;
+  hashtbl
